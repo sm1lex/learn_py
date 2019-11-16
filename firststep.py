601bd1a929bb3c68717e26b443af86b2eee2949b
sm1lex <48415697+sm1lex@users.noreply.github.com>
2019-11-16 14:17:25 +0300

    install and test python jedi-vim

--- firststep.py
+++ firststep.py
@@ -1,2 +1,4 @@
 #First programm begin with <education can breverly the chain>
+name = "Smilove"
 print(' Education can break the chain of breverly')
+print(" {} install jedi-vim for python ".format(name))
