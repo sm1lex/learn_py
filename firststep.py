0886df9e15de06f283d90b61a8631a67e71318fa
sm1lex <48415697+sm1lex@users.noreply.github.com>
2019-11-16 23:21:38 +0300

    Write some ways how we can work and format with strin in python

--- firststep.py
+++ firststep.py
@@ -1,4 +1,12 @@
 #First programm begin with <education can breverly the chain>
+age = 31
 name = "Smilove"
-print(' Education can break the chain of breverly')
+
 print(" {} install jedi-vim for python ".format(name))
+print('{age} years name {name} i start using git/vim/python'.format(age=age, name=name))
+print(f'{age} years name {name} i start using git/vim/python')
+print(' Number my fucking girls maybe {number}'.format(number=20))
+# If you wish write your strin in another number of line
+print('This text in the first line \n This text in the second line')
+print('''This triple quotes give me flexibility for writing my text
+        every where, and i don't think about /\\''')
