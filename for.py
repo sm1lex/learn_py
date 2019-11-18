18b65a8312660e1231f86ffea120160ab6d0f044
sm1lex <48415697+sm1lex@users.noreply.github.com>
2019-11-18 21:43:24 +0300

    Learning and use control flow statement if-elif-else/for-in(break)-else(when break else don't execute)/while-else(continue)

---
+++ for.py
@@ -0,0 +1,5 @@
+#example for...in loop
+for i in range(1, 20, 2):
+    print(i)
+else:
+    print('for...in loop is over')
---
+++ while_break.py
@@ -0,0 +1,12 @@
+#break statement in while loop
+while True:
+    s = input('Enter something : ')
+    if s == 'quit':
+        break
+    if len(s) < 3:
+        print('To small length')
+        continue
+    print ('Length of the string is', len(s))
+else:
+    print ('Common every body i hack the system')
+print('Done')
