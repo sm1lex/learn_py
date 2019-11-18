b47cb83505da794332546febe6a2d8ea1b855a46
sm1lex <48415697+sm1lex@users.noreply.github.com>
2019-11-18 11:44:15 +0300

    some example by operands in python3

--- indentation.py
+++ indentation.py
@@ -1,4 +1,6 @@
 #Example how use indentation in python 
 i = 5
- print("Local variable is ", i)#This line have Indentation Error, because have whitespace begining line
 print('I repeat value', i)
+ b = 4    
+ print("Local variable is ", b)#This line have Indentation Error, because have whitespace begining line
+
---
+++ operands.py
@@ -0,0 +1,23 @@
+#some example use operands ip python
+print(2 + 2)#summ
+print(5 - 2)#substruction
+print(2 * 2)#multiply
+print(3 ** 3)#exponentiation
+print(13 / 3)#devide
+print(13 // 3)#devide and floor
+print(9 // 1.81)#if one of the value is float you have answer in float
+print(13 % 3)#modulo
+print(2 << 2)#left bite shift 0010 << 2 = 1000
+print(11 >> 1)#right shift 1011 >> 1 = 0101
+print(5 & 3)#bit wise AND = 1
+print(5 | 3)#bit wise OR = 7
+print(5 ^ 3)#bit wise XOR = 7
+print(~5)#bit wise Inversion of x is -(x+1) = -6
+print(7 > 5 > 3)#greater or less than = false or true
+print(7 <= 7)#greater or less and equal than
+print(7 == 7)#equal
+print(5 != 7)#not equal
+x = True
+y = False
+#boolean <not/and/or> f
+print((33+ 3)* 2)
