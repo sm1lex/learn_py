77c63384a00b1956059236daf49708e871bf258f
sm1lex <48415697+sm1lex@users.noreply.github.com>
2019-12-29 17:39:30 +0300

    check word for palindrome

--- io_nextinput.py
+++ io_nextinput.py
@@ -2,8 +2,16 @@ input_str = input('Enter some word for check: ')
 
 
 
-str_intuple = ['some work ! wit a']
-del_symbol =  [?, !,:, ;, -, (, {}, [], !, ' ', _, ',']
+str_intuple = ('some work ! wit a')
+del_symbol =  ['?', '!',':', ';', '-', '(', '{}', '[]', '!', '' '', '_', ',']
 #print(str_intuple & del_symbol)
-print(str_intuple in del_symbol)
+#print(str_intuple in del_symbol)
+
+for i in str_intuple:
+    if i == '!':
+
+        print(i)
+        del i
+
+    print(i, end=' ')
 
