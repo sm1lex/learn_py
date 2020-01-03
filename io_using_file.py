a2c8f1e20b855c01b3e3a740a7c349e42fa75fb4
sm1lex <48415697+sm1lex@users.noreply.github.com>
2020-01-03 22:07:03 +0300

    IO basic sample by python3

---
+++ io_using_file.py
@@ -0,0 +1,24 @@
+# basic principles of using files
+
+ipsum = '''\
+Programming is fun, when the programm is done
+if you wanna have a good and interesting job:
+    use Python everywere!
+'''
+
+
+# open or creat file and assign this object <f> variable
+
+f = open('poem.txt', 'w')
+f.write(ipsum)
+f.close()
+
+
+f = open('poem.txt', 'r')
+while True:
+    string = f.readline()
+    if len(string) == 0:
+        break
+
+    print(string, end = '')
+f.close()
